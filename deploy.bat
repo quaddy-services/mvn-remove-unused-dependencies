10dccfbc6923aaa9e2f5aeee68d77f1f8f240161
quaddy-services <mail@quaddy-services.de>
2012-11-20 20:30:14 +0100

    proxy

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,7 @@ rem gpg --list-keys
 call mvn clean deploy -Dgpg.passphrase=%quaddypassphrase% -P sonatype-oss-release
 
 echo on 
+rem for relase behind a proxy:
+rem $ git config --global http.proxy http://proxyuser:proxypwd@proxy.server.com:8080
+
 rem mvn release:prepare release:perform -Darguments=-Dgpg.passphrase=%quaddypassphrase% -P sonatype-oss-release
