cd3f802a44a6e99c7485de32ebcc56983574a9e2
Stefan Cordes <mail@quaddy-services.de>
2012-11-20 21:45:58 +0100

    ftp://ftp.gnupg.org/gcrypt/binary/gnupg-w32cli-1.4.11.exe

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,9 @@
 echo on
+
+rem Install
+rem ftp://ftp.gnupg.org/gcrypt/binary/gnupg-w32cli-1.4.11.exe
+rem gpg --gen-key
+rem gpg --keyserver hkp://pool.sks-keyservers.net --send-keys C6EED57A
 rem see http://www.sonatype.com/people/2010/01/how-to-generate-pgp-signatures-with-maven/
 rem gpg --list-keys
 call mvn clean deploy -Dgpg.passphrase=%quaddypassphrase% -P sonatype-oss-release
